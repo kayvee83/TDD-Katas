7c0267a67c75ee1489df8998bcfa5c0a60762a73
Vinoth Kabirdoss <vkabirdo@ford.com>
2017-08-04 14:53:21 -0400

    Initial commit for TDD kata

---
+++ src/main/java/com/vinoth/tddkata/TddkataApplication.java
@@ -0,0 +1,12 @@
+package com.vinoth.tddkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddkataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddkataApplication.class, args);
+	}
+}
